fab23f110bc7072cd040b8cecf671a64f25fac5c
“NguyenTungSon0503” <sonmeliodas0503@gmail.com>
2022-10-28 15:10:42 +0700

    add_api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,14 @@
 class HomeController < ApplicationController
   def index
-    require 'net/http'
+   require 'net/http'
     require 'json'
-    @url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=YOURKEYHERE&start=1&limit=5000&convert=USD'
+    @url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=fd04676f-2762-4500-9f39-c0290aa0c3c8&start=1&limit=5000&convert=USD'
     @uri = URI(@url)
-    @response = NET::HTTP.get(@uri)
+    @response = Net::HTTP.get(@uri)
     @coins = JSON.parse(@response)
+    @my_coins = ["BTC","ETH","USDT","BNB",]
   end
-  
   def about
+
   end
 end
