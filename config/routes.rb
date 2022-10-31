bdfcbcf9fe5b92acdf2e3c2c0b8de3b63fc1abf6
“NguyenTungSon0503” <sonmeliodas0503@gmail.com>
2022-10-31 23:07:55 +0700

    add_table @tranbinhminh1403

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'home/about'
   get 'home/lookup'
+  get 'home/table'
   post '/home/lookup' => 'home/lookup'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
