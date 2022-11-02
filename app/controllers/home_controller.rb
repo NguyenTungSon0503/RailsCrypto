b3e5ff0fc5e49dd300d6bb8848cfb44785aec26b
“NguyenTungSon0503” <sonmeliodas0503@gmail.com>
2022-11-02 09:54:30 +0700

    first_commit_searchbyname

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
-  def index
+  def index #show infor 100 coins
     require 'net/http'
     require 'json'
-    @url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=fd04676f-2762-4500-9f39-c0290aa0c3c8&start=1&limit=5000&convert=USD'
+    @url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=fd04676f-2762-4500-9f39-c0290aa0c3c8&start=1&limit=100&convert=USD'
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @coins = JSON.parse(@response)
@@ -10,10 +10,10 @@ class HomeController < ApplicationController
   end
 
 
-  def lookup
+  def lookup #search by symbol
     require 'net/http'
     require 'json'
-    @url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=fd04676f-2762-4500-9f39-c0290aa0c3c8&start=1&limit=5000&convert=USD'
+    @url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=fd04676f-2762-4500-9f39-c0290aa0c3c8&start=1&limit=100&convert=USD'
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @lookup_coin = JSON.parse(@response)
@@ -24,14 +24,42 @@ class HomeController < ApplicationController
       @symbol = @symbol.upcase
     end
   end
-  def table
+  def table #show table 100 rows
     require 'net/http'
     require 'json'
-    @url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=fd04676f-2762-4500-9f39-c0290aa0c3c8&start=1&limit=5000&convert=USD'
+    @url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=fd04676f-2762-4500-9f39-c0290aa0c3c8&start=1&limit=100&convert=USD'
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @coins = JSON.parse(@response)
     @my_coins = ["BTC","ETH","USDT","BNB","ADA"]
   end
   
+  def search #search by name
+    require 'net/http'
+    require 'json'
+    @url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=fd04676f-2762-4500-9f39-c0290aa0c3c8&start=1&limit=100&convert=USD'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @search_coin = JSON.parse(@response)
+    @name = params[:name]
+    @error = "Not found coin"
+    @a = 0
+    if @name
+     if @name.count(" ") == 0
+      @final = @name.capitalize()
+     elsif @name.count(" ") == 1
+      array = @name.split
+      first = array[0].capitalize()
+      second = array[1].capitalize()
+      @final = first + " " + second
+     elsif @name.count(" ") == 2
+      array = @name.split
+      first = array[0].capitalize()
+      second = array[1].capitalize()
+      third = array[2].capitalize()
+      @final = first + " " + second + " " + third
+     end
+
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,14 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'home/about'
   get 'home/lookup'
+  get 'home/search'
   get 'home/table'
   get 'home/readme'
   post '/home/lookup' => 'home/lookup'
+  post '/home/search' => 'home/search'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "home#search"
 end
