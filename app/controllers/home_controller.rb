a96cb6089b09f6fb6448c00bab268483665343b9
“NguyenTungSon0503” <sonmeliodas0503@gmail.com>
2022-11-02 11:03:08 +0700

    update_searchbyinput

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,11 +17,11 @@ class HomeController < ApplicationController
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @lookup_coin = JSON.parse(@response)
-    @symbol = params[:sym]
+    @input = params[:name_or_sym]
     @error = "Not found coin"
     @a = 0
-    if @symbol 
-      @symbol = @symbol.upcase
+    if @input 
+      @input = @input.downcase
     end
   end
   def table #show table 100 rows
@@ -32,21 +32,6 @@ class HomeController < ApplicationController
     @response = Net::HTTP.get(@uri)
     @coins = JSON.parse(@response)
     @my_coins = ["BTC","ETH","USDT","BNB","ADA"]
-  end
-  
-  def search #search by name
-    require 'net/http'
-    require 'json'
-    @url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=fd04676f-2762-4500-9f39-c0290aa0c3c8&start=1&limit=100&convert=USD'
-    @uri = URI(@url)
-    @response = Net::HTTP.get(@uri)
-    @search_coin = JSON.parse(@response)
-    @name = params[:name]
-    @error = "Not found coin"
-    @a = 0
-    if @name
-      
-    end
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,11 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'home/about'
   get 'home/lookup'
-  get 'home/search'
   get 'home/table'
   get 'home/readme'
   post '/home/lookup' => 'home/lookup'
-  post '/home/search' => 'home/search'
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#search"
+  root "home#index"
 end
